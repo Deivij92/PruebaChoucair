59d87661bb9316bd8dbda57f8960ed27af913e11
Deivij92 <43120794+Deivij92@users.noreply.github.com>
2021-02-02 14:54:17 -0500

    Solved <List>
    Solver error in the import librery

--- PruebaChoucair/src/test/java/co/com/choucair/certificacion/pruebachoucair/stepdefinitions/ChoucairAcademyStepDefinitions.java
+++ PruebaChoucair/src/test/java/co/com/choucair/certificacion/pruebachoucair/stepdefinitions/ChoucairAcademyStepDefinitions.java
@@ -9,10 +9,11 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import io.vavr.collection.List;
 import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
+
+import java.util.List;
 
 public class ChoucairAcademyStepDefinitions {
     @Before
